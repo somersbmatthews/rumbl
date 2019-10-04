204b6e6b26bdff061015dbaf74601f014798065a
Somers Matthews <somersbmatthews@gmail.com>
2019-10-03 22:23:42 -0500

    completed show in UserController

--- lib/rumbl_web/controllers/user_controllers.ex
+++ lib/rumbl_web/controllers/user_controllers.ex
@@ -5,4 +5,9 @@ defmodule RumblWeb.UserController do
       users = Rumbl.Repo.all(Rumbl.User)
          render conn, "index.html", users: users
    end
+
+   def show(conn, %{"id" => id}) do
+      user = Rumbl.Repo.get(Rumbl.User, id)
+         render conn, "show.html", user: user
+   end
 end
